f6d3967cfe43a57ffe93b4e5a48a57b3e30ba1bd
ClassCMS <git@classcms.com>
2024-12-26 22:25:11 +0800

    模型页面文件名禁用特殊字符

--- class/cms/route.php
+++ class/cms/route.php
@@ -72,6 +72,9 @@ class cms_route {
         if(!isset($route_add_query['classview'])) {
             $route_add_query['classview']='';
         }
+        if(strpos($route_add_query['classview'],'<') || strpos($route_add_query['classview'],'>') || strpos($route_add_query['classview'],'|') || strpos($route_add_query['classview'],':') || strpos($route_add_query['classview'],'?') || strpos($route_add_query['classview'],'"') || strpos($route_add_query['classview'],'/') || strpos($route_add_query['classview'],'\\')) {
+            Return false;
+        }
         if(empty($route_add_query['classfunction']) && empty($route_add_query['classview'])) {
             Return false;
         }
@@ -109,6 +112,11 @@ class cms_route {
         }
         if(isset($route_edit_query['classfunction'])) {
             $route_edit_query['classfunction']=trim($route_edit_query['classfunction']);
+        }
+        if(isset($route_edit_query['classview'])){
+            if(strpos($route_edit_query['classview'],'<') || strpos($route_edit_query['classview'],'>') || strpos($route_edit_query['classview'],'|') || strpos($route_edit_query['classview'],':') || strpos($route_edit_query['classview'],'?') || strpos($route_edit_query['classview'],'"') || strpos($route_edit_query['classview'],'/') || strpos($route_edit_query['classview'],'\\')) {
+                Return false;
+            }
         }
         Return update($route_edit_query);
     }
